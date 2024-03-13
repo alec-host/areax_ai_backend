b4a0c0d74d7e86dc65271c5941e24b4b7eddf25c
alec-host <irualec@yahoo.com>
2024-03-13 13:52:44 +0300

    3rd commit - remove aiven info

--- app/db/db.js
+++ app/db/db.js
@@ -1,7 +1,6 @@
 const Sequelize = require("sequelize");
 const { DATABASE_NAME, DATABASE_USER, DATABASE_PASS, DATABASE_HOST } = require("../constants/app_constants");
 
-
 const sequelize = new Sequelize(
    DATABASE_NAME,
    DATABASE_USER,
@@ -16,23 +15,5 @@ sequelize.authenticate().then(() => {
 }).catch((error) => {
    console.error('Unable to connect to the database: ', error);
 });
-
-/*
-const sequelize = new Sequelize(
-   "defaultdb",
-   "avnadmin",
-   "AVNS_IS6hpdABYmThBvpWY4t",
-    {
-      host: "mysql://avnadmin:AVNS_IS6hpdABYmThBvpWY4t@mysql-c76337f-area-x2024.a.aivencloud.com:22694/defaultdb?ssl-mode=REQUIRED",
-      dialect: 'mysql'
-    }
-  );
-sequelize.authenticate().then(() => {
-   console.log('Connection: was successful.');
-}).catch((error) => {
-   console.error('Unable to connect to the database: ', error);
-});
-*/
-
 
 module.exports = sequelize;

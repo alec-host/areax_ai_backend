f7baf234e343f81637a6e38e10aad4dcae142212
alec-host <irualec@yahoo.com>
2024-05-23 23:10:22 +0300

    30th commit: modified error.timeout.routes.js script

--- app/routes/error/error.timeout.routes.js
+++ app/routes/error/error.timeout.routes.js
@@ -1,6 +1,6 @@
 exports.errorTimeOutHandler = async(err,req,res,next) => {
     if(req.timedout) {
-      res.status(504).send('Request timed out');
+      res.status(504).json({ success: false, error: true, message:'Request timed out' });
     }else {
        next();
     }
